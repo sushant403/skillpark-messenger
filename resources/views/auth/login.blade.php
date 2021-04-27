48689a6cbc70c1e6ad73606e60771b42a8ccd2a9
Sushant Poudel <sushantpaudel@gmail.com>
2021-04-27 23:16:31 +0545

    Error Message Bug Fixes.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <!-- ========== MAIN ========== -->
 <main id="content" role="main">
     <!-- Form -->
-    <div class="d-flex align-items-center position-relative vh-lg-100">
+    <div class="d-flex align-items-center position-relative">
         <div class="col-lg-5 col-xl-4 d-none d-lg-flex align-items-center bg-dark vh-lg-100 px-0"
             style="background-image: url(/images/svg/components/abstract-shapes-20.svg);">
             <div class="w-100 p-5">
@@ -98,9 +98,9 @@
         </div>
 
         <div class="container">
+            @include('components.message')
             <div class="row no-gutters">
                 <div class="col-md-8 col-lg-7 col-xl-6 offset-md-2 offset-lg-2 offset-xl-3 space-top-3 space-lg-0">
-                    @include('components.message')
                     <!-- Form -->
                     <form method="POST" action="{{ route('login') }}" class="js-validate">
                         @csrf
--- resources/views/components/message.blade.php
+++ resources/views/components/message.blade.php
@@ -7,17 +7,20 @@
 @endif
 
 @if(session()->has('error'))
-<div class="alert alert-danger alert-dismissible fade" role="alert">
-  <h4 class="alert-heading">Oh Oo!</h4>
+<div class="alert alert-danger alert-dismissible position-absolute w-auto z-index-9999 fade show" role="alert">
+  <h4 class="alert-heading"><i class="fas fa-info-circle mt-1 mr-1"></i> Uh-Oh! Error!</h4>
   <p class="text-inherit">{{ session()->get('error') }}</p>
   <hr />
   <p class="text-inherit mb-0">To register and Login to Skillpark, please follow this <a
       href="https://skillpark.herokuapp.com">link</a></p>
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    x
+  </button>
 </div>
 @endif
 
 @if($errors->count() > 0)
-<div class="alert alert-danger alert-dismissible fade">
+<div class="alert alert-danger alert-dismissible">
   <h4 class="alert-heading">Oh Oo!</h4>
   <ul class="list-unstyled">
     @foreach($errors->all() as $error)
